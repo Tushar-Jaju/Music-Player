a4f7b167d198835340d993746a99ce73f361f8fe
Tushar-Jaju <jajutushar5@gmail.com>
2024-04-01 11:34:05 +0530

    retrieving songs from array

--- script.js
+++ script.js
@@ -1,10 +1,46 @@
+const playList = [
+  {
+    src: "./media/let me down slowly.mp3",
+    title: "Let me down slowly",
+    artist: "Alec Benjamin",
+    img: "./media/let me down slowly.jpeg",
+  },
+  {
+    src: "./media/unity.mp3",
+    title: "Unity",
+    artist: "Alan Walker",
+    img: "./media/unity.jpeg",
+  },
+  {
+    src: "./media/spectre.mp3",
+    title: "Spectre",
+    artist: "Alan Walker",
+    img: "./media/spectre.jpg",
+  },
+];
+
+let index = 0;
+
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
+const songImg = document.querySelector(".song-img");
+
 const song = document.getElementById("song");
 const progress = document.getElementById("progress");
 const ctrlIcon = document.getElementById("ctrlIcon");
 
+function loadMusic() {
+  title.innerHTML = playList[index].title;
+  artist.innerHTML = playList[index].artist;
+  songImg.src = playList[index].img;
+  song.src = playList[index].src;
+}
+
 song.onloadedmetadata = function () {
   progress.max = song.duration;
-  progress.value = song.currentTime;
+  setInterval(() => {
+    progress.value = song.currentTime;
+  }, 500);
 };
 
 function playPause() {
@@ -19,10 +55,20 @@ function playPause() {
   }
 }
 
-if (song.play()) {
-  setInterval(() => {
-    progress.value = song.currentTime;
-  }, 500);
+function playNext() {
+  if (index < playList.length - 1) {
+    index++;
+    loadMusic();
+    song.play();
+  }
+}
+
+function playPrev() {
+  if (index > 0) {
+    index--;
+    loadMusic();
+    song.play();
+  }
 }
 
 progress.onchange = function () {
@@ -37,3 +83,5 @@ song.addEventListener("ended", () => {
   ctrlIcon.classList.add("fa-play");
   ctrlIcon.classList.remove("fa-pause");
 });
+
+loadMusic();
